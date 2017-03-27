1ae8de74bad1c4ba877a20e62fe6d21faa3678fc
Emmanuel Castro <emmanuel.castro@laposte.net>
2017-03-27 22:36:42 +0200

    Essais qui marche bien

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const sourcemaps = require('gulp-sourcemaps')
 const babel = require('gulp-babel')
 const clean = require('gulp-clean')
 const gulpSequence = require('gulp-sequence')
-// const exec = require('child_process').exec
 
 // Global error report
 process.on('uncaughtException', function (error) {
@@ -20,27 +19,13 @@ gulp.task('clean', () => {
     .pipe(clean())
 })
 
-const map = require('map-stream')
-function log (key) {
-  return map(function (x, callback) {
-  //  console.log(key, x)
-    callback(null, x)
-  })
-}
-
 // Build for NodeJS
 gulp.task('build', () => {
-  // const gulpCombine = require('gulp-combine')
-
   return gulp.src('src/**/*.js')
-    .pipe(log('a'))
     .pipe(sourcemaps.init())
-    .pipe(log('b'))
     .pipe(babel())
-    .pipe(log('c'))
     .pipe(sourcemaps.write())
-    .pipe(log('d'))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('dist'))
 })
 
 // Continuous build for NodeJS
