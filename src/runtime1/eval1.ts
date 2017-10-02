9083de098a5d68288286c41f79130d0371bfb961
Emmanuel Castro <emmanuel.castro@laposte.net>
2017-10-02 12:38:14 +0200

    Amélioration des exceptions chaînées (bis)

--- src/runtime1/eval1.ts
+++ src/runtime1/eval1.ts
@@ -49,7 +49,17 @@ export class Closure {
   }
 }
 
-class EvalError extends XError { }
+class EvalError extends XError {
+
+  expression?: Expr
+
+  constructor (msg?: string, expression?: Expr, cause?: Error) {
+    super(msg || _.truncate(`${expression}`, { length: 250 }))
+    this.expression = expression
+    this.cause = cause
+  }
+
+}
 
 class PrimitiveError extends EvalError { }
 
@@ -78,126 +88,131 @@ const stats = new Map<Expr, number>()
  * Simple strict evaluation
  */
 export default function eval1 (expr: Expr, env: Env): Value | Promise<Value> {
-  // console.log('+++', debugInfo(expr))
-  stats.set(expr, (stats.get(expr) || 0) + 1)
+  try {
+    // console.log('+++', debugInfo(expr))
+    stats.set(expr, (stats.get(expr) || 0) + 1)
 
-  switch (expr.typ) {
-    case eVar.typ:
-      return env.lookup(expr.varId)
+    switch (expr.typ) {
+      case eVar.typ:
+        return env.lookup(expr.varId)
 
-    case eLiteral.typ:
-      const l = expr.value
-      if (typeof l === 'symbol') {
-        const x = primitives[l]
-        if (x == null) {
-          throw new Error(`Undefined primitive #${Symbol.keyFor(l)}`)
-        }
-        return x
-      } else {
-        return l
-      }
-
-    case eLet.typ: {
-      const entries = expr.defs.entries()
-      const newEnv: Env = new Env(new Map(), env)
-      for (const [k, subExpr] of entries) {
-        // Launch all computations
-        newEnv.frame.set(k, eval1(subExpr, newEnv))   // TODO: simple - no letrec - fuzzy semantics
-      }
-      const result = eval1(expr.body, newEnv)
-      return result
-    }
-
-    case eLambda.typ:
-      return new Closure(expr, env)
-
-    case eIfElse.typ: // eval if...
-      const ifExpr = expr
-      return then(eval1(expr.ifClause, env), b => {
-        if (typeof b === 'boolean') {
-          if (b) {
-            return eval1(ifExpr.thenClause, env)
-          } else {
-            return eval1(ifExpr.elseClause, env)
+      case eLiteral.typ:
+        const l = expr.value
+        if (typeof l === 'symbol') {
+          const x = primitives[l]
+          if (x == null) {
+            throw new Error(`Undefined primitive #${Symbol.keyFor(l)}`)
           }
+          return x
         } else {
-          throw new EvalError('Illegal value in if: ' + String(b) + ' - ' + expr.toString())
+          return l
         }
-      })
 
-    default:
-    case eApply.typ:
-      const applyExpr = expr
-      return then(eval1(expr.operator, env), (op) => {
-        const ops = applyExpr.operands
-        try {
-          if (op instanceof Closure) {
-            const frame = new Map()
-            const params = op.lambda.params
-            if (params.length !== ops.length) {
-              throw new EvalError(`Argument count ${ops.length} differs from parameter count ${params.length}`)
-            }
+      case eLet.typ: {
+        const entries = expr.defs.entries()
+        const newEnv: Env = new Env(new Map(), env)
+        for (const [k, subExpr] of entries) {
+          // Launch all computations
+          newEnv.frame.set(k, eval1(subExpr, newEnv))   // TODO: simple - no letrec - fuzzy semantics
+        }
+        const result = eval1(expr.body, newEnv)
+        return result
+      }
 
-            for (let i = 0; i < ops.length; i++) {
-              frame.set(params[i], eval1(ops[i], env))
-            }
-            const newEnv: Env = new Env(frame, op.defEnv)
+      case eLambda.typ:
+        return new Closure(expr, env)
 
-            return eval1(op.lambda.body, newEnv)
-
-          } else {
-            // Special case: primitive, array or object access
-
-            // Parameter evaluation
-            const args = []  // inlined Array.map
-            for (let i = 0; i < ops.length; i++) {
-              args.push(eval1(ops[i], env))
-            }
-
-            // Primitive function (from Literal)
-            if (typeof op === 'function') {
-              if (op.length !== args.length) {
-                if ((op as any).varArgs) {
-                  throw new EvalError(`Argument count ${args.length} differs from parameter count ${op.length} (P)`)
-                }
-              }
-              try {
-                return callPrimitive(op, args, 0) // ATTENTION: in-place modification of args
-
-              } catch (e) {
-                console.error(expr.debugInfo())
-                throw new PrimitiveError(e)
-              }
+      case eIfElse.typ: // eval if...
+        const ifExpr = expr
+        return then(eval1(expr.ifClause, env), b => {
+          if (typeof b === 'boolean') {
+            if (b) {
+              return eval1(ifExpr.thenClause, env)
             } else {
-              // Other cases: array or object
+              return eval1(ifExpr.elseClause, env)
+            }
+          } else {
+            throw new EvalError(`Illegal value in if: ${b} - ${expr}`)
+          }
+        })
 
-              if (args.length === 1) {
-                // Array-like access or method
-                return then(args[0], (arg1) => {
-                  if (typeof arg1 === 'number') {
-                    // Array access
-                    if (Array.isArray(op)) {
-                      return op[arg1]
-                    }
-                  }
-                  if (typeof arg1 === 'string') {
-                    // Method call
-                    const generic: any = op
-                    if (generic[arg1] !== undefined) {
-                      return generic[arg1]
-                    }
-                  }
-                  throw new EvalError('No method or special application available')
-                })
+      default:
+      case eApply.typ:
+        const applyExpr = expr
+        return then(eval1(expr.operator, env), (op) => {
+          const ops = applyExpr.operands
+          try {
+            if (op instanceof Closure) {
+              const frame = new Map()
+              const params = op.lambda.params
+              if (params.length !== ops.length) {
+                throw new EvalError(`Argument count ${ops.length} differs from parameter count ${params.length}`)
               }
 
-              throw new EvalError('No method or special application available')
+              for (let i = 0; i < ops.length; i++) {
+                frame.set(params[i], eval1(ops[i], env))
+              }
+              const newEnv: Env = new Env(frame, op.defEnv)
+
+              return eval1(op.lambda.body, newEnv)
+
+            } else {
+              // Special case: primitive, array or object access
+
+              // Parameter evaluation
+              const args = []  // inlined Array.map
+              for (let i = 0; i < ops.length; i++) {
+                args.push(eval1(ops[i], env))
+              }
+
+              // Primitive function (from Literal)
+              if (typeof op === 'function') {
+                if (op.length !== args.length) {
+                  if ((op as any).varArgs) {
+                    throw new EvalError(`Argument count ${args.length} differs from parameter count ${op.length} (P)`)
+                  }
+                }
+                try {
+                  return callPrimitive(op, args, 0) // ATTENTION: in-place modification of args
+
+                } catch (e) {
+                  console.error(expr.debugInfo())
+                  throw new PrimitiveError(e)
+                }
+              } else {
+                // Other cases: array or object
+
+                if (args.length === 1) {
+                  // Array-like access or method
+                  return then(args[0], (arg1) => {
+                    if (typeof arg1 === 'number') {
+                      // Array access
+                      if (Array.isArray(op)) {
+                        return op[arg1]
+                      }
+                    }
+                    if (typeof arg1 === 'string') {
+                      // Method call
+                      const generic: any = op
+                      if (generic[arg1] !== undefined) {
+                        return generic[arg1]
+                      }
+                    }
+                    throw new EvalError('No method or special application available')
+                  })
+                }
+
+                throw new EvalError('No method or special application available')
+              }
             }
+          } catch (e) {
+            if (e instanceof EvalError && e.expression === expr) throw e
+            throw new EvalError(undefined, expr, e)
           }
-        } catch (e) {
-          if (e instanceof EvalError) throw e
-          throw new EvalError('', e)
-        }
-      })
+        })
+    }
+  } catch (e) {
+    if (e instanceof EvalError && e.expression === expr) throw e
+    throw new EvalError(undefined, expr, e)
   }
 }
